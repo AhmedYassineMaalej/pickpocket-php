872f4f05cca98fa6b62ee59d4645fb295cc09cdc
AhmedYassineMaalej <ahmedyessinemaalej@gmail.com>
2026-05-28 15:08:17 +0100

    fix bug with catalog dropdown on /catalog

--- views/pages/catalog.php
+++ views/pages/catalog.php
@@ -55,7 +55,6 @@ require __DIR__ . "/../fragments/product_section.php";
             </div>
         </div>
     <script src="/js/sidebar.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="/js/catalog.js"></script>
     <script src="/js/bookmark_button.js"></script>
     </body>
